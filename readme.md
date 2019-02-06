a5f98e3ed21445c2877ce236fdbb3582a62dd6f4
Brian Dillingham <brian@brians-mbp.home>
2019-02-06 12:01:35 -0500

    initial

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Nova Index Textarea
+
+[![Latest Version on Github](https://img.shields.io/github/release/dillingham/nova-index-textarea.svg?style=flat-square)](https://packagist.org/packages/dillingham/nova-index-textarea)
+[![Total Downloads](https://img.shields.io/packagist/dt/dillingham/nova-index-textarea.svg?style=flat-square)](https://packagist.org/packages/dillingham/nova-index-textarea)
+
+Nova package that adds methods to Textarea for index view.
+
+### Install
+
+```bash
+composer require dillingham/nova-index-textarea
+```
+
+### Usage
+
+```php
+use Laravel\Nova\Fields\Textarea;
+```
+```php
+Textarea::make('Description')->showOnIndex()->limit(10)
+```
+
+`limit()` accepts a second paramter to override '...'
---
+++ src/FieldServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dillingham\NovaTextarea;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Fields\Textarea;
+use Illuminate\Support\ServiceProvider;
+
+class FieldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Textarea::macro('showOnIndex', function(){
+            $this->showOnIndex = true;
+
+            return $this;
+        });
+
+        Textarea::macro('limit', function($amount, $ending = '...') {
+            $this->displayUsing(function() use($amount, $ending) {
+                return str_limit($this->value, $amount, $ending);
+            });
+
+            return $this;
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
