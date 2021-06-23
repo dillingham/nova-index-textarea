3f53fd89bd54386cfc9a516051c40a9d0a64732e
Metrix Information Services <43896192+metrixinfo@users.noreply.github.com>
2021-06-22 20:08:00 -0400

    Update str_limit to use the Str class
    Make this compatible with newer versions of Laravel.

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -5,6 +5,7 @@ namespace Dillingham\NovaTextarea;
 use Laravel\Nova\Nova;
 use Laravel\Nova\Fields\Textarea;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class FieldServiceProvider extends ServiceProvider
@@ -25,7 +26,7 @@ class FieldServiceProvider extends ServiceProvider
         Textarea::macro('limit', function($amount, $ending = '...') {
             if(resolve(NovaRequest::class)->resourceId == null) {
                 $this->displayUsing(function() use($amount, $ending) {
-                    return str_limit($this->value, $amount, $ending);
+                    return Str::limit($this->value, $amount, $ending);
                 });
             }
 
